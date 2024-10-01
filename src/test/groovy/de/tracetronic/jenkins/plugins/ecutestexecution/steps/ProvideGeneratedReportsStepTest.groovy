89bcc4b1f749abd7bd0715c7184b3df964f9f70e
maxe <Max.Ehrlich@tracetronic.de>
2024-10-01 17:12:05 +0200

    add unit tests (#159)

--- src/test/groovy/de/tracetronic/jenkins/plugins/ecutestexecution/steps/ProvideGeneratedReportsStepTest.groovy
+++ src/test/groovy/de/tracetronic/jenkins/plugins/ecutestexecution/steps/ProvideGeneratedReportsStepTest.groovy
@@ -5,7 +5,70 @@ import hudson.EnvVars
 import hudson.Launcher
 import hudson.model.TaskListener
 
+import java.util.zip.ZipEntry
+import java.util.zip.ZipOutputStream
+
 class ProvideGeneratedReportsStepTest extends Specification {
+    def createTestZip(){
+        def reportZip  = File.createTempFile("test", ".zip")
+        reportZip.deleteOnExit()
+        ZipOutputStream zos = new ZipOutputStream(new FileOutputStream(reportZip))
+        zos.putNextEntry(new ZipEntry("html/"))
+        zos.closeEntry()
+        zos.putNextEntry(new ZipEntry("html/file1.html"))
+        zos.closeEntry()
+        zos.putNextEntry(new ZipEntry("json/"))
+        zos.closeEntry()
+        zos.putNextEntry(new ZipEntry("json/file1.json"))
+        zos.closeEntry()
+        zos.close()
+        return reportZip
+    }
+
+    def "Test processReport with all files included"() {
+        given:
+            def step = new ProvideGeneratedReportsStep()
+            def reportDirName = "testreport"
+            def outDirPath = "/tmp/output"
+            def listener = Mock(TaskListener)
+            def logger = Mock(PrintStream)
+        and:
+            listener.logger >> logger
+
+        when:
+            def result = step.processReport(createTestZip(), reportDirName, outDirPath, listener)
+
+        then:
+            result.collect { it.replaceAll("\\\\", "/") } == extractedFiles
+            loggerCalled * logger.println("[WARNING] Could not find any matching generated report files in testreport!")
+
+        where:
+            scenario               | extractedFiles                                   | loggerCalled
+            "all files included"   | ["/tmp/output/testreport/html.zip", "/tmp/output/testreport/json.zip"] | 0
+    }
+
+    def "Test processReport with all files excluded"() {
+            given:
+                def step = new ProvideGeneratedReportsStep()
+                step.setIncludePattern("nothing matches")
+                def reportDirName = "testreport"
+                def outDirPath = "/tmp/output"
+                def listener = Mock(TaskListener)
+                def logger = Mock(PrintStream)
+            and:
+                listener.logger >> logger
+
+            when:
+                def result = step.processReport(createTestZip(), reportDirName, outDirPath, listener)
+
+            then:
+                result == extractedFiles
+                loggerCalled * logger.println("[WARNING] Could not find any matching generated report files in testreport!")
+
+            where:
+                scenario               | extractedFiles                           | loggerCalled
+                "files missing"        | []                                       | 1
+        }
 
     def "Test DescriptorImpl returns correct values"() {
         given:
