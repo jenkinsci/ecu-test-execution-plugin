3b1cb2b491fd2ba5347ac1355979f03a07709205
maxe <Max.Ehrlich@tracetronic.de>
2024-10-07 12:58:02 +0200

    add tbdd notes (#159) - adjust for user selection of reports via checkboxes - adjust ziputil usage - fix non existent report generator options

--- src/main/groovy/de/tracetronic/jenkins/plugins/ecutestexecution/steps/ProvideGeneratedReportsStep.groovy
+++ src/main/groovy/de/tracetronic/jenkins/plugins/ecutestexecution/steps/ProvideGeneratedReportsStep.groovy
@@ -11,71 +11,65 @@ import hudson.EnvVars
 import hudson.Extension
 import hudson.Launcher
 import hudson.model.TaskListener
-import org.apache.tools.ant.types.selectors.SelectorUtils
+import hudson.util.ListBoxModel
 import org.jenkinsci.plugins.workflow.steps.StepDescriptor
 import org.kohsuke.stapler.DataBoundConstructor
 import org.kohsuke.stapler.DataBoundSetter
-import java.util.zip.ZipEntry
-import java.util.zip.ZipInputStream
 
 class ProvideGeneratedReportsStep extends AbstractProvideExecutionFilesStep {
-    private static final String ICON_NAME = 'testreport'
+    private static final String ICON_NAME = 'testreport' //TODO get generate report icon
     private static final String OUT_DIR_NAME = "generated-ecu.test-reports"
     private static final String SUPPORT_VERSION = "2024.3"
-    private static final String DEFAULT_INCLUDE_PATTERN = "*"
-    private String includePattern
+    private List<String> selectedReportTypes
 
     @DataBoundConstructor
     ProvideGeneratedReportsStep() {
         super()
-        this.includePattern = DEFAULT_INCLUDE_PATTERN
+        this.selectedReportTypes = GenerateReportsStep.DescriptorImpl.REPORT_GENERATORS.collect()
         iconName = ICON_NAME
         outDirName = OUT_DIR_NAME
         supportVersion = SUPPORT_VERSION
     }
 
-    String getIncludePattern() {
-        return this.includePattern
+    List<String> getSelectedReportTypes() {
+        return this.selectedReportTypes
     }
 
     @DataBoundSetter
-    void setIncludePattern(String includePattern) {
-        this.includePattern = (includePattern != null) ? includePattern : DEFAULT_INCLUDE_PATTERN;
+    void setSelectedReportTypes(List<String> selectedReportTypes) {
+        this.selectedReportTypes = (selectedReportTypes != null) ? selectedReportTypes : GenerateReportsStep.DescriptorImpl.REPORT_GENERATORS.collect();
     }
 
 
     protected ArrayList<String> processReport(File reportZip, String reportDirName, String outDirPath, TaskListener listener) {
         ArrayList<String> reportPaths = []
-        HashMap<String, ArrayList<String>> folderMap = [:]
-        new ZipInputStream(new FileInputStream(reportZip)).withCloseable { zipInputStream ->
-            ZipEntry entry
-            while ((entry = zipInputStream.getNextEntry()) != null) {
-                if (SelectorUtils.match(includePattern, entry.name) && entry.name.contains('/')) {
-                    def folderName = entry.name.substring(0, entry.name.indexOf('/'))
-                    if (reportDirName.contains(folderName)) {
-                        // always exclude execution report
-                        continue
-                    }
-                    def folderEntryPaths = folderMap.getOrDefault(folderName, [])
-                    folderEntryPaths.add(entry.getName())
-                    folderMap.put(folderName, folderEntryPaths)
-                }
+        selectedReportTypes.each { reportType ->
+            def folderEntryPaths = ZipUtil.getAllMatchingPaths(reportZip, "${reportType}*/**")
+            if (folderEntryPaths) {
+                def folderName = folderEntryPaths[0].substring(0, folderEntryPaths[0].indexOf('/'))
+                def outputFile = new File("${outDirPath}/${reportDirName}/${folderName}.zip")
+                outputFile.parentFile.mkdirs()
+                reportPaths.add(ZipUtil.recreateWithFilesOfType(reportZip, folderEntryPaths, outputFile))
             }
         }
-        folderMap.each { k, v ->
-            def outputFile = new File("${outDirPath}/${reportDirName}/${k}.zip")
-            outputFile.parentFile.mkdirs()
-            reportPaths.add(ZipUtil.recreateWithFilesOfType(reportZip, v, outputFile))
-        }
-
         if (reportPaths.size() == 0) {
             listener.logger.println("[WARNING] Could not find any matching generated report files in ${reportDirName}!")
         }
+
         return reportPaths
     }
 
     @Extension
     static final class DescriptorImpl extends StepDescriptor {
+
+        static ListBoxModel getSelectedReportTypes() {
+            ListBoxModel reportGenerators = new ListBoxModel();
+            GenerateReportsStep.DescriptorImpl.REPORT_GENERATORS.collect { reportType ->
+                reportGenerators.add(reportType, reportType)
+            }
+            return reportGenerators;
+        }
+
         @Override
         String getFunctionName() {
             'ttProvideGeneratedReports'
--- src/test/groovy/de/tracetronic/jenkins/plugins/ecutestexecution/ETV2ContainerTest.groovy
+++ src/test/groovy/de/tracetronic/jenkins/plugins/ecutestexecution/ETV2ContainerTest.groovy
@@ -231,7 +231,7 @@ class ETV2ContainerTest extends ETContainerTest {
                     withEnv(['ET_API_HOSTNAME=${etContainer.host}', 'ET_API_PORT=${etContainer.getMappedPort(ET_PORT)}']) {
                         ttRunPackage testCasePath: 'test.pkg'
                         ttGenerateReports 'HTML'
-                        ttProvideGeneratedReports includePattern: 'HTML*/**'
+                        ttProvideGeneratedReports publishConfig: [allowMissing: true], selectedReportTypes: ['NOMATCH']
                     }
                 }
                 """.stripIndent()
