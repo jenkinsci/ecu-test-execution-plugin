1e8ce79890525aaadbf376912b65aa48156fd03b
maxe <Max.Ehrlich@tracetronic.de>
2024-12-19 10:56:25 +0100

    add some review notes (#205) - combine some methods - remove comments - use setup methods

--- src/test/groovy/de/tracetronic/jenkins/plugins/ecutestexecution/configs/PublishConfigTest.groovy
+++ src/test/groovy/de/tracetronic/jenkins/plugins/ecutestexecution/configs/PublishConfigTest.groovy
@@ -4,38 +4,6 @@ import spock.lang.Specification
 
 class PublishConfigTest extends Specification {
 
-//    def "setTimeout sets timeout to 0 if negative value is provided"() {
-//        given:
-//        def publishConfig = new PublishConfig()
-//
-//        when:
-//        publishConfig.setTimeout(-1)
-//
-//        then:
-//        publishConfig.getTimeout() == 0
-//    }
-//
-//    def "setTimeout sets timeout to the provided value if it is positive"() {
-//        given:
-//        def publishConfig = new PublishConfig()
-//
-//        when:
-//        publishConfig.setTimeout(100)
-//
-//        then:
-//        publishConfig.getTimeout() == 100
-//    }
-//
-//    def "setTimeout sets timeout to 0 if value is 0"() {
-//        given:
-//        def publishConfig = new PublishConfig()
-//
-//        when:
-//        publishConfig.setTimeout(0)
-//
-//        then:
-//        publishConfig.getTimeout() == 0
-//    }
     def "setTimeout sets timeout correctly based on input value"() {
         given:
             def publishConfig = new PublishConfig()
