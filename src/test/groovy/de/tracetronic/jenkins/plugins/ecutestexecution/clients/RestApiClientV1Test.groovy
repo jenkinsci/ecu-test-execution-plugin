e9c57ea4241203786febfc0890f986e82edeac78
maxe <Max.Ehrlich@tracetronic.de>
2024-12-19 15:27:28 +0100

    add minimal api client unit tests (#205)

---
+++ src/test/groovy/de/tracetronic/jenkins/plugins/ecutestexecution/clients/RestApiClientV1Test.groovy
@@ -0,0 +1,37 @@
+package de.tracetronic.jenkins.plugins.ecutestexecution.clients
+
+import de.tracetronic.cxs.generated.et.client.api.v1.ApiStatusApi
+import de.tracetronic.cxs.generated.et.client.model.v1.SimpleMessage
+import spock.lang.Specification
+
+import java.util.concurrent.TimeoutException
+
+class RestApiClientV1Test extends Specification {
+
+    def "waitForAlive should throw TimeoutException when timeout is exceeded"() {
+        given:
+            def client = new RestApiClientV1("localhost","80")
+            client.timeoutExceeded = true
+
+        when:
+            client.waitForAlive(1)
+
+        then:
+            thrown(TimeoutException)
+    }
+    def "waitForAlive"() {
+        given:
+            def client = new RestApiClientV1("localhost","80")
+            def statusApi = GroovySpy(ApiStatusApi, global:true)
+            statusApi.isAlive() >> new SimpleMessage().message(given)
+        when:
+            def result = client.waitForAlive(1)
+        then:
+            result == expected
+
+        where:
+            given  | expected
+            "Alive"| true
+            ""     | false
+    }
+}
