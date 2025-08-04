86c41badb6340aeadc4295871ea7188c28f2cdb3
maxe <Max.Ehrlich@tracetronic.de>
2025-08-04 11:59:44 +0200

    add license server var (#246)

--- src/test/groovy/de/tracetronic/jenkins/plugins/ecutestexecution/ContainerTest.groovy
+++ src/test/groovy/de/tracetronic/jenkins/plugins/ecutestexecution/ContainerTest.groovy
@@ -8,8 +8,8 @@ package de.tracetronic.jenkins.plugins.ecutestexecution
 import spock.lang.Specification
 
 class ContainerTest extends Specification {
-
-    protected static final String ET_V1_IMAGE_NAME = System.getenv('DOCKER_REGISTRY_OLD') "/ecu-test/linux:" + System.getenv('ET_V1_VERSION') // TODO remove once V1 obsolete
+    protected static final String ET_LICENSE_SERVER = System.getenv('tracet_LICENSE')
+    protected static final String ET_V1_IMAGE_NAME = System.getenv('DOCKER_REGISTRY_OLD') + "/ecu-test/linux:" + System.getenv('ET_V1_VERSION') // TODO remove once V1 obsolete
     protected static final String ET_V2_IMAGE_NAME = System.getenv('DOCKER_REGISTRY_ARTIFACTORY') + "/tracetronic/ecu.test:" + System.getenv('ET_V2_VERSION')
     protected static final int ET_PORT = 5050
     protected static final String ET_WS_PATH = "/app/workspace"
