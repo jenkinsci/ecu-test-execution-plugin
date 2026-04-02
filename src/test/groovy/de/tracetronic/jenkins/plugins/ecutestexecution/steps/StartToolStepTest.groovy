4b1b0f1a02b792db312e99afabc88adf62443ae9
Tilo Werdin <tilo.werdin@tracetronic.de>
2026-04-02 10:52:35 +0200

    i262: improve tests with explicit error messages and ignore path shortening on linux

--- src/test/groovy/de/tracetronic/jenkins/plugins/ecutestexecution/steps/StartToolStepTest.groovy
+++ src/test/groovy/de/tracetronic/jenkins/plugins/ecutestexecution/steps/StartToolStepTest.groovy
@@ -1,18 +1,23 @@
 package de.tracetronic.jenkins.plugins.ecutestexecution.steps
 
+import spock.lang.IgnoreIf
 import spock.lang.Specification
+import spock.lang.Unroll
 
 class StartToolStepTest extends Specification {
 
+    @Unroll
     def "setTimeout sets timeout correctly based on input value"() {
         given:
         def startToolStep = new StartToolStep("ecu.test")
 
         when:
         startToolStep.setTimeout(inputValue)
+        int actualTimeout = startToolStep.getTimeout()
 
         then:
-        startToolStep.getTimeout() == expectedTimeout
+        assert actualTimeout == expectedTimeout:
+            "Unexpected timeout for input '#{inputValue}'. Expected #{expectedTimeout}, but was #{actualTimeout}."
 
         where:
         inputValue | expectedTimeout
@@ -21,9 +26,15 @@ class StartToolStepTest extends Specification {
         100        | 100
     }
 
+    @Unroll
     def "sanitizeToolNameForLogFile keeps readable names and neutralizes unsafe parts"() {
-        expect:
-        StartToolStep.createLogFile("", inputValue, "").getName() == expectedToolName
+        when:
+        File file = StartToolStep.createLogFile("", inputValue, "")
+        String actualToolName = file.getName()
+
+        then:
+        assert actualToolName == expectedToolName:
+            "Unexpected sanitized tool name for input '#{inputValue}'. Expected '#{expectedToolName}', but was '#{actualToolName}' (absolutePath='#{file.absolutePath}')."
 
         where:
         inputValue                  | expectedToolName
@@ -36,15 +47,18 @@ class StartToolStepTest extends Specification {
         '\u5DE5\u5177 \u540D\u79F0' | '\u5DE5\u5177_\u540D\u79F0'
     }
 
+    @IgnoreIf({ !os.isWindows() })
     def "sanitizeToolNameForLogFile truncates long names to max length"() {
         given:
         String longName = 'A' * (StartToolStep.MAX_WINDOWS_FILE_PATH_LENGTH + 20)
 
         when:
         File file = StartToolStep.createLogFile("", longName, "")
+        int actualPathLength = file.absolutePath.length()
 
         then:
-        file.absolutePath.length() == StartToolStep.MAX_WINDOWS_FILE_PATH_LENGTH
+        assert actualPathLength == StartToolStep.MAX_WINDOWS_FILE_PATH_LENGTH:
+            "Unexpected log file path length. Expected #{StartToolStep.MAX_WINDOWS_FILE_PATH_LENGTH}, but was #{actualPathLength} (absolutePath='#{file.absolutePath}')."
     }
 
 }
